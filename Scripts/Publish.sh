cb418a52235df213fccba3c9c1d1acd08ad8efc5
Egor Vashkevich <goshikvash@gmail.com>
2024-09-15 16:16:18 +0300

    feat: updates

--- Scripts/Publish.sh
+++ Scripts/Publish.sh
@@ -4,16 +4,16 @@ set -e
 
 SCRIPT_DIR="$(realpath "$(dirname "${BASH_SOURCE[0]}" )")"
 HEADING_DIR="$(dirname "${SCRIPT_DIR}")"
-PROJECT_DIR="$(dirname "${SCRIPT_DIR}")"
+PROJECT_DIR="$(dirname "${HEADING_DIR}")"
 
-echo "cd ${HEADING_DIR}"
+echo "[INFO] cd ${HEADING_DIR}"
 cd "${HEADING_DIR}"
 git add -A
 git commit -m "feat: updates"
 git push
 
 # Add new commits from submodule
-echo "cd ${PROJECT_DIR}"
+echo "[INFO] cd ${PROJECT_DIR}"
 cd "${PROJECT_DIR}"
 git add Headings
 git commit -m "update Headings"
